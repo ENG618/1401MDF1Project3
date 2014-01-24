8b59d563879bb6be09ea753f29a0a3fe6fa42b34
eng618 <eng618@fullsail.edu>
2014-01-23 22:28:10 -0500

    Initial Commit

---
+++ Project3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project3
+//
+//  Created by Eric Garcia on 1/23/14.
+//  Copyright (c) 2014 Full Sail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
